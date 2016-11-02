2f1aba0e18194ff0be8d19035e7702363a5a133d
vtkimchi <vtkchi1297@gmail.com>
2016-11-02 15:34:20 +0700

    Init Project

---
+++ src/JavaClass.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author Chi Vo - T153556
+ *
+ */
+public class JavaClass extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaClass f = new JavaClass();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+
+	}
+
+}
